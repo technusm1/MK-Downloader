fa28f0d7a81785d1fb51b4b8737b073409dbec5c
Maheep Kumar Kathuria <me@maheepk.net>
2022-06-24 22:19:11 +0530

    Initial Commit

---
+++ MK Downloader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MK Downloader
+//
+//  Created by Maheep Kumar Kathuria on 24/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MK Downloader/MK_DownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MK_DownloaderApp.swift
+//  MK Downloader
+//
+//  Created by Maheep Kumar Kathuria on 24/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MK_DownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
